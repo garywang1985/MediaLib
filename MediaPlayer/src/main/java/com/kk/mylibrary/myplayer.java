eb1fe20bc33ed069bf334bdfd2559b4c1e2aec3a
Guolai <wgl@arcvideo.com>
2018-03-07 14:36:08 +0800

    init version
    Signed-off-by: Guolai <wgl@arcvideo.com>

---
+++ MediaPlayer/src/main/java/com/kk/mylibrary/myplayer.java
@@ -0,0 +1,17 @@
+package com.kk.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by wgl on 2018/1/25.
+ */
+
+public class myplayer {
+    private static String LOG_TAG = "myplayer";
+
+    private int version = 1;
+    myplayer()
+    {
+        Log.d(LOG_TAG,"constructor");
+    }
+}
